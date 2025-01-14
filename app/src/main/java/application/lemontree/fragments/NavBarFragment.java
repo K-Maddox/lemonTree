9d242887464378f2bfa3df58342263d34ce7299e
K-Maddox <kmad5568@gmail.com>
2025-01-14 21:17:43 +1100

    Add fragments package and nav bar fragment class

---
+++ app/src/main/java/application/lemontree/fragments/NavBarFragment.java
@@ -0,0 +1,22 @@
+package application.lemontree.fragments;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import application.lemontree.R;
+import application.lemontree.activities.MessageActivity;
+import application.lemontreee.activities.OfferActivity;
+import application.lemontree.activities.Profile;
+import application.lemontree.activities.WantedActivity;
+
+public class NavBarFragment extends Fragment {
+}
