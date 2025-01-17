220efa65c1ac482f34ade07d3027fcbb5df8d160
K-Maddox <kmad5568@gmail.com>
2025-01-17 19:02:19 +1100

    Add settings fragment

---
+++ app/src/main/java/application/lemontree/fragments/SettingsFragment.java
@@ -0,0 +1,29 @@
+package application.lemontree.fragments;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import application.lemontree.R;
+import application.lemontree.activities.WelcomeActivity;
+
+public class SettingsFragment {
+}
