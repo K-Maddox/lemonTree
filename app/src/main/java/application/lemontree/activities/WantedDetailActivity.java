aa4d83763eba9098282b148b59de213cba57100a
K-Maddox <kmad5568@gmail.com>
2025-01-26 11:29:17 +1100

    Add declarations and oncreate to wanted detail activity

--- app/src/main/java/application/lemontree/activities/WantedDetailActivity.java
+++ app/src/main/java/application/lemontree/activities/WantedDetailActivity.java
@@ -37,4 +37,65 @@ import application.lemontree.R;
 import application.lemontree.fragments.NavBarFragment;
 
 public class WantedDetailActivity extends AppCompatActivity {
+    private ImageView wantImageView;
+    private TextView wantTitleTextView;
+    private TextView wantDescriptionTextView;
+    private TextView createdAtTextView;
+    private TextView categoryTextView;
+    private TextView createdByUsernameTextView, wantAvailableDateTextView;
+    private TextView archivedTextView;
+    private ImageView profileImageView;
+    private Button chatButton;
+    private Button archiveButton;
+    private String currentUserId;
+    private String title;
+    private String wantUserId;
+    private String wantUserName;
+    private String wantUserURL;
+    private String wantId;
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_wanted_in_detail);
+
+        wantId = getIntent().getStringExtra("wantId");
+        if (wantId == null) {
+            Log.e("WantedDetailActivity", "wantId is null");
+            finish();
+            return;
+        }
+
+        // Initialize views
+        wantImageView = findViewById(R.id.wantedImageView);
+        wantTitleTextView = findViewById(R.id.wantedTitleTextView);
+        categoryTextView = findViewById(R.id.categoryTextView);
+        wantDescriptionTextView = findViewById(R.id.wantedDescriptionTextView);
+        wantAvailableDateTextView = findViewById(R.id.availableDateTextView);
+        createdAtTextView = findViewById(R.id.createdAtTextView);
+        createdByUsernameTextView = findViewById(R.id.createdByUsernameTextView);
+        profileImageView = findViewById(R.id.profileImageView);
+        chatButton = findViewById(R.id.chatButton);
+        archiveButton = findViewById(R.id.archiveButton);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+            Log.e("WantedDetailActivity", "No user is logged in");
+            return;
+        }
+
+        currentUserId = currentUser.getUid();
+        getWantDetails(wantId);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.nav_container, new NavBarFragment())
+                .commit();
+    }
 }
