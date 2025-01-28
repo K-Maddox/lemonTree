73deefd64ec4a75b350c11d2b12060d09b31138a
K-Maddox <kmad5568@gmail.com>
2025-01-28 18:37:07 +1100

    Add methods to filter helper

--- app/src/main/java/application/lemontree/helpers/FilterHelper.java
+++ app/src/main/java/application/lemontree/helpers/FilterHelper.java
@@ -5,12 +5,24 @@ import androidx.fragment.app.FragmentManager;
 import java.util.function.Consumer;
 
 public class FilterHelper {
+
     // This method will show the filter dialog and apply the filter callback when a radius is selected
     public static void showFilterDialog(FragmentManager fragmentManager, FilterDialogFragment dialog, int radius, Consumer<Integer> filterCallback) {
         if (dialog == null) {
             dialog = new FilterDialogFragment();
         }
 
+        // Pass the current radius as an argument to the dialog fragment
+        Bundle args = new Bundle();
+        args.putInt("radius", radius);
+        dialog.setArguments(args);
 
+        // Set the listener for when the filter is applied
+        dialog.setOnFilterAppliedListener(newRadius -> {
+            filterCallback.accept(newRadius);  // Pass the new radius to the callback
+        });
+
+        // Show the filter dialog
+        dialog.show(fragmentManager, "filterDialog");
     }
 }
