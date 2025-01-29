509b490aa683f1d6acb5fe07ab3464ce367562be
K-Maddox <kmad5568@gmail.com>
2025-01-29 21:33:44 +1100

    Add imports to filter dialogue fragment helper

--- app/src/main/java/application/lemontree/helpers/FilterDialogFragment.java
+++ app/src/main/java/application/lemontree/helpers/FilterDialogFragment.java
@@ -1,4 +1,20 @@
 package application.lemontree.helpers;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
+
+import application.lemontree.R;
+
 public class FilterDialogFragment {
 }
