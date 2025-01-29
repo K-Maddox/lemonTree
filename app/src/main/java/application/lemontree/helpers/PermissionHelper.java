ed575d6d33f5ffed5a7ec278c196a58e98584033
K-Maddox <kmad5568@gmail.com>
2025-01-29 21:29:23 +1100

    Add constructor for permission helper

--- app/src/main/java/application/lemontree/helpers/PermissionHelper.java
+++ app/src/main/java/application/lemontree/helpers/PermissionHelper.java
@@ -1,4 +1,17 @@
 package application.lemontree.helpers;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class PermissionHelper {
+
+    private Activity activity;
+
+    public PermissionHelper(Activity activity) {
+        this.activity = activity;
+    }
+
 }
