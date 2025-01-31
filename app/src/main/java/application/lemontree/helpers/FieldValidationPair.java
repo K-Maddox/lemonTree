8c556193555e83ddcb7ee1131d1024a62ef5442d
K-Maddox <kmad5568@gmail.com>
2025-01-31 11:28:59 +1100

    Add methods to field validation pair helper

--- app/src/main/java/application/lemontree/helpers/FieldValidationPair.java
+++ app/src/main/java/application/lemontree/helpers/FieldValidationPair.java
@@ -4,4 +4,11 @@ import android.widget.EditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class FieldValidationPair {
+    public EditText editText;
+    public TextInputLayout inputLayout;
+
+    public FieldValidationPair(EditText editText, TextInputLayout inputLayout) {
+        this.editText = editText;
+        this.inputLayout = inputLayout;
+    }
 }
