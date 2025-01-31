0981f459b2ffbc7fe0f6c808f5e05f75c85993fc
K-Maddox <kmad5568@gmail.com>
2025-01-31 11:30:51 +1100

    Add imports to message adapter

--- app/src/main/java/application/lemontree/adapter/MessageAdapter.java
+++ app/src/main/java/application/lemontree/adapter/MessageAdapter.java
@@ -1,4 +1,23 @@
 package application.lemontree.adapter;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.List;
+
+import application.lemontree.R;
+
 public class MessageAdapter {
 }
