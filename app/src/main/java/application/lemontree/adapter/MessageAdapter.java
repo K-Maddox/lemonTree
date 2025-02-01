2e93ffc04a3a21422576d59edf6b8fe468290765
K-Maddox <kmad5568@gmail.com>
2025-02-01 12:16:58 +1100

    Add methods to message adapter

--- app/src/main/java/application/lemontree/adapter/MessageAdapter.java
+++ app/src/main/java/application/lemontree/adapter/MessageAdapter.java
@@ -40,4 +40,54 @@ public class MessageAdapter {
                 .inflate(R.layout.item_message, parent, false);
         return new MessageViewHolder(view);
     }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        // Bind data to views in the item_message layout
+        QueryDocumentSnapshot chat = messageList.get(position);
+        String offerId = chat.getString("offerId");
+
+        holder.itemNameTextView.setText(chat.getString("title"));
+
+        holder.lastMessageTextView.setText(chat.getString("lastMessage"));
+
+
+        // Get the other participant's name
+        List<String> participants = (List<String>) chat.get(
+                "participants");
+        List<String> participantNames = (List<String>) chat.get(
+                "participantsNames");
+        List<String> profilePictureURLs = (List<String>) chat.get(
+                "profilePictureURLs");
+
+        String pictureURL = null;
+        String otherParticipantName = null;
+        String otherParticipantId = null;
+
+
+        for (int i = 0; i < participants.size(); i++) {
+            String participantId = participants.get(i);
+            if (!participantId.equals(mAuth.getCurrentUser().getUid())) {
+                // This is the other participant
+                otherParticipantId = participantId;
+                otherParticipantName = participantNames.get(i);
+                pictureURL = profilePictureURLs.get(i);
+                break;
+            }
+        }
+
+        Glide.with(holder.itemView.getContext())
+                .load(pictureURL)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(R.drawable.lemon)
+                .into(holder.imageView);
+
+        Log.i(TAG,
+                mAuth.getCurrentUser().getUid() + " " + otherParticipantName + " " + otherParticipantId + " " + pictureURL);
+
+        holder.fromUserTextView.setText(otherParticipantName != null ? otherParticipantName : "Unknown");
+
+        // click to chat detail
+        holder.itemView.setOnClickListener(v -> listener.onItemClick(chat));
+    }
 }
