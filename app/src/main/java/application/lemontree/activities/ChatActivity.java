2d9bfdd4217646b279ed87299ffaaa9a4d2b4498
K-Maddox <kmad5568@gmail.com>
2025-02-03 20:57:39 +1100

    Add imports to chat activity

--- app/src/main/java/application/lemontree/activities/ChatActivity.java
+++ app/src/main/java/application/lemontree/activities/ChatActivity.java
@@ -1,4 +1,27 @@
 package application.lemontree.activities;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import application.lemontree.R;
+import application.lemontree.adapters.ChatMessageAdapter;
+import application.lemontree.models.ChatMessage;
+
 public class ChatActivity {
 }
