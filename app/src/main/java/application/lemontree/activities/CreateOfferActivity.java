757e3c1c44f8a23679647baa600d0906d6ae8e32
K-Maddox <kmad5568@gmail.com>
2025-02-15 17:23:39 +1100

    Add methods to create offer activity

--- app/src/main/java/application/lemontree/activities/CreateOfferActivity.java
+++ app/src/main/java/application/lemontree/activities/CreateOfferActivity.java
@@ -458,6 +458,23 @@ public class CreateOfferActivity extends AppCompatActivity {
         return mime.getExtensionFromMimeType(c.getType(uri));
     }
 
+    // Create image file
+    private File createImageFile() throws IOException {
+        // Create an image file name with timestamp
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,   /* prefix */
+                ".jpg",          /* suffix */
+                storageDir       /* directory */
+        );
+
+        // Save a file path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     // Capture photo
     private void dispatchTakePictureIntent() {
         // Get location before capturing the photo
