880b024189aa80e975a6ce8321cec491b67bf9b5
K-Maddox <kmad5568@gmail.com>
2025-02-17 19:35:39 +1100

    Add imports to create want activity

--- app/src/main/java/application/lemontree/activities/CreateWantActivity.java
+++ app/src/main/java/application/lemontree/activities/CreateWantActivity.java
@@ -1,6 +1,37 @@
 package application.lemontree.activities;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import application.lemontree.R;
+import application.lemontree.helpers.FieldValidationPair;
+import application.lemontree.models.Want;
+import application.lemontree.services.LocationGetService;
 
 public class CreateWantActivity extends AppCompatActivity {
 }
