10099becacf313f0868bbeaa61ed71e2aae104bd
K-Maddox <kmad5568@gmail.com>
2025-02-27 15:43:13 +1100

    Add imports to settings activity

--- app/src/main/java/application/lemontree/activities/SettingsActivity.java
+++ app/src/main/java/application/lemontree/activities/SettingsActivity.java
@@ -1,4 +1,13 @@
 package application.lemontree.activities;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import application.lemontree.R;
+import application.lemontree.fragments.NavBarFragment;
+import application.lemontree.fragments.SettingsFragment;
+
 public class SettingsActivity {
 }
