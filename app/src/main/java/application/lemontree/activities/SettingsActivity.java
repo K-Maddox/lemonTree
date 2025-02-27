66aa252cadbeee4faae98a6b3ce34e7e57cf0622
K-Maddox <kmad5568@gmail.com>
2025-02-27 15:44:05 +1100

    Add imports to settings activity

--- app/src/main/java/application/lemontree/activities/SettingsActivity.java
+++ app/src/main/java/application/lemontree/activities/SettingsActivity.java
@@ -9,5 +9,26 @@ import application.lemontree.R;
 import application.lemontree.fragments.NavBarFragment;
 import application.lemontree.fragments.SettingsFragment;
 
-public class SettingsActivity {
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //Set up fragment for tabbar
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.nav_container, new NavBarFragment())
+                .commit();
+
+        //Set up fragment for Settings (see SettingsFragment.java)
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.settings_container, new SettingsFragment())
+                    .commit();
+        }
+    }
 }
